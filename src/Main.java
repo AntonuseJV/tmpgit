82f619dd90a6a9a93a2389371c903b2ed85c5314
AntonuseJV <anton954@rambler.ru>
2022-10-07 16:02:21 +0300

    Method for trimmed average added

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
         SalesManager manager = new SalesManager(sales);
         System.out.println("Max:" + manager.max());
+        System.out.println("Min:" + manager.min());
+
+        SalesManager day1 = new SalesManager(new int[]{35, 38, 36, 40, 39, 45, 43});
+        System.out.println("Максимальное количество проданных позиций в течение дня " + day1.max() + " шт");
+        System.out.println("Среднее количество проданных позиций в течение дня " + day1.average() + " шт");
+
     }
 }
