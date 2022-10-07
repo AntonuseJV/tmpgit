24e7e55f081cc3ecf1ba7f1945f5d0c92df153bd
AntonuseJV <anton954@rambler.ru>
2022-10-07 15:25:46 +0300

    Added demonstration in Main and README

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] sales = {100,30,410,15};
+
+        SalesManager manager = new SalesManager(sales);
+        System.out.println("Max:" + manager.max());
+    }
+}
